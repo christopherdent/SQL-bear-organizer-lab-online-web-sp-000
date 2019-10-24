b2045500845829fdfb40c5a05d9c1b8b51676d1d
Christopher Dent <christopherdent01@gmail.com>
2019-10-24 02:04:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,8 +19,10 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears AS MOST_FREQUENT LIMIT 1"
+  "SELECT count(*) FROM bears WHERE color = MOST_FREQUENT;"
   #"SELECT color FROM bears ORDER BY color DESC LIMIT 1;"
+ 
+  
 end
 
 def counts_number_of_bears_with_goofy_temperaments
