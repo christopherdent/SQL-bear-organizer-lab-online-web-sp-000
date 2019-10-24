b5d3001793e4d8477a69b82371ca3f41e2b37c70
Christopher Dent <christopherdent01@gmail.com>
2019-10-24 01:17:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,14 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 3, "male", "brown", "aggressive", true);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 3, "male", "brown", "aggressive", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 6, "female", "brown", "aggressive", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 3, "male", "brown", "aggressive", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Grinch", 3, "male", "brown", "aggressive", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Melissa", 3, "female", "brown", "aggressive", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 9, "female", "brown", "aggressive", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, null, 19, "male", "brown", "aggressive", true);
